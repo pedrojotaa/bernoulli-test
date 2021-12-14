c3164d986a4663ca36e8f849e4a48a2e9c088afd
Joao Pedro <eng.joao.pedro@outlook.com>
2021-12-14 08:08:21 -0300

    new method to list users

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,27 @@
 const Posts = require('../model/posts')
+const login = require('../middleware/login')
 
 module.exports = app => {
     app.post('/cadastro', (req, res) => {
 
         const dados = req.body
 
-        Posts.add(dados, res)
+        Posts.cadastro(dados, res)
 
     })
 
     app.post('/login', (req, res) => {
-
+     
         const email = req.body.email
         const password = req.body.password
 
         Posts.login(email, password, res)
+
+    })
+
+    app.get('/lista',login.obrigatorio, (req, res) => {
+        console.log(req.usuario)
+        Posts.lista(res)
 
     })
 }
